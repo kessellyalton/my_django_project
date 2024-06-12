cea74712fd1ef29949f5a90cb050f041914a5a90
Alton Kesselly <kessellyalton@outlook.com>
2024-06-12 01:42:44 +0000

    Second major update to my project

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'debug_toolbar',
+    'blog.apps.BlogConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -48,8 +49,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
 ]
+
+
+
 
 ROOT_URLCONF = "mysite.urls"
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+
